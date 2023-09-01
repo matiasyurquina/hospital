3e59e136e8fa92fad656693682e88ebb45f0128a
Matias <matias_cx@yahoo.com.ar>
2023-09-02 02:09:54 +0400

    Debug false
    Debug false

--- hospital/settings.py
+++ hospital/settings.py
@@ -11,7 +11,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-71u#i8z6#@wgn+q)&6^8y!ge=*#&^c&)45jk55(1@)$bh!#!y-'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = ['*']
 
